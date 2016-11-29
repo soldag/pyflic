56c46996b9d1cbe267927da01e3e78539197da73
Sören Oldag <soeren_oldag@freenet.de>
2016-11-29 02:06:20 +0100

    Add fliclib.py and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyflic',
+      version='1.0',
+      description='Python library to connect to and interact with Flic buttons.',
+      author='Sören Oldag',
+      author_email='soeren_oldag@freenet.de',
+      url='https://github.com/soldag/pyflic'
+     )
