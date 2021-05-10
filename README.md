533441f4b2308ada47262827dc4d6d3d9870803a
Sören Oldag <soeren_oldag@freenet.de>
2021-05-10 23:19:48 +0200

    Prepare for publishing to PyPI

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # pyflic
 
-This repository only mirrors the flic python client library from https://github.com/50ButtonsEach/fliclib-linux-hci and just exists for testing purposes until there is an official PyPI package. 
+This repository only mirrors the flic python client library from https://github.com/50ButtonsEach/fliclib-linux-hci and just exists until there is an official PyPI package.
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 
 setup(name='pyflic',
-      version='0.4',
+      version='0.4.0',
       description='Python library to connect to and interact with Flic buttons.',
-      author='Sören Oldag',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='soldag',
       author_email='soeren_oldag@freenet.de',
       url='https://github.com/soldag/pyflic',
-      packages=['pyflic']
+      license='CC0 1.0 Universal',
+      packages=find_packages(),
      )
