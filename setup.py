75b1a9e503c25d252dcd2286b5d632aca96a908d
Sören Oldag <soeren_oldag@freenet.de>
2021-05-10 23:01:48 +0200

    Update client libs to v0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 
 setup(name='pyflic',
-      version='0.4.0',
+      version='0.5.0',
       description='Python library to connect to and interact with Flic buttons.',
       long_description=long_description,
       long_description_content_type='text/markdown',
